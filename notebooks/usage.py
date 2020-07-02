e20b54efd8792681e22573369e52937c5d06aedd
thomasgoelles <thomas.goelles@v2c2.at>
2020-07-02 09:14:12 +0000

    better doc

--- notebooks/usage.py
+++ notebooks/usage.py
@@ -22,6 +22,13 @@
 import lidar
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [20, 10]
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+get_ipython().run_line_magic('config', 'IPCompleter.greedy=True')
+
 
 # Ignore the warning, which comes from the rospy package.
 
@@ -43,33 +50,34 @@ testset = lidar.Dataset(testbag,lidar_name="ouster",keep_zeros=False)
 # This reads the bagfile into the Dataset.
 # Dataset only reads frames from the bagfile if needed, in order to save memory and make it possible to work which huge bagfiles.
 
-# In[14]:
+# In[ ]:
 
 
 print(testset)
 
 
-# In[15]:
+# In[ ]:
 
 
 len(testset)
 
 
 # In order to see whats availble use "tab" to see the availble properties and methods. Alterantivly, use help(), dir(), and the documentation.
+# Also shift tab is nice inside jupyter lab.
 
-# In[17]:
+# In[ ]:
 
 
 help(testset)
 
 
-# In[18]:
+# In[ ]:
 
 
 dir(testset)
 
 
-# In[19]:
+# In[ ]:
 
 
 testset.topics_in_bag
@@ -77,22 +85,26 @@ testset.topics_in_bag
 
 # You can also work diretly with the bag if needed.
 
-# In[21]:
+# In[ ]:
 
 
 type(testset.bag)
 
 
 # # Work with Frames
+# 
+# They are based on pandas dataframes and pyntcloud.
+# This was necessary since, no pointcloud library currently support to store automotive lidar data which consists of more than just y,x,z and maybe R,G,B
+# 
 # First grab the first frame in the dataset:
 
-# In[22]:
+# In[ ]:
 
 
 testframe = testset[0]
 
 
-# In[13]:
+# In[ ]:
 
 
 print(testframe)
@@ -121,6 +133,98 @@ testframe.plot_interactive()
 
 
 testframe.plot_interactive(backend="pyntcloud")
+
+
+# In[ ]:
+
+
+testframe.plot_interactive(color="range", point_size=0.5)
+
+
+# ## Working with pointcouds
+# The frame consists maily of the properties "data" and "points".
+
+# In[ ]:
+
+
+testframe.data
+
+
+# So data contains everything as a pandas dataframe. With all its power.
+
+# In[ ]:
+
+
+testframe.data.describe()
+
+
+# In[ ]:
+
+
+testframe.data.hist();
+
+
+# Now a closer look a the points. 
+
+# In[ ]:
+
+
+testframe.points
+
+
+# So its a Pyntcloud object https://pyntcloud.readthedocs.io/en/latest/PyntCloud.html which in turn is also based on Dataframes with many methods for pointclouds.
+# In order to access the dataframe use this:
+
+# In[ ]:
+
+
+testframe.points.points
+
+
+# You can also work with the pointcloud with open3d
+
+# In[ ]:
+
+
+open3d_points = testframe.get_open3d_points()
+
+
+# In[ ]:
+
+
+open3d_points.get_max_bound()
+
+
+# ## Pointcloud processing with build in methods
+# Although you can do a lot with just data and points, on its own the Frame object has methods build in for processing, which in turn return a frame object. The use the power of dataframes, pyntcloud and open3d.
+# 
+
+# In[ ]:
+
+
+newframe = testframe.limit("x",-5,5)
+
+
+# In[ ]:
+
+
+newframe.data.describe()
+
+
+# So this is now a smaller Frame with x ranging from -5 to  5
+
+# These command can also be chained together
+
+# In[ ]:
+
+
+newframe2 = testframe.limit("x",-5,5).limit("y",-5,5)
+
+
+# In[ ]:
+
+
+newframe2.data.describe()
 
 
 # In[ ]:
