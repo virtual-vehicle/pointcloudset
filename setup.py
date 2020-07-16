ec4abbea8737480bca0a0727f0b35e798a46ec56
tgoelles <thomas.goelles@gmail.com>
2020-07-16 15:15:56 +0200

    changed to version 0.1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="lidar",
-    packages=["lidar", "lidar.file", "lidar.convert", "lidar.plot", "lidar.processing"],
-    version="0.0.8",
-    author="Thomas Gölles",
+    packages=["lidar", "lidar.file", "lidar.convert", "lidar.plot"],
+    version="0.1.0",
+    author="Thomas Gölles, Sarah Haas",
     author_email="thomas.goelles@v2c2.at",
-    description="Analyse automotive lidar data",
+    description="Analyse automotive lidar data stored in ROS bagfiles",
     python_requires=">=3.7",
 )
