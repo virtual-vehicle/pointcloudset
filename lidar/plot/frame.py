fbf4a6e21bd71395ffb795a096c91d009e565d11
tgoelles <thomas.goelles@gmail.com>
2020-07-16 10:01:57 +0200

    more documentation WIP

--- lidar/plot/frame.py
+++ lidar/plot/frame.py
@@ -1,3 +1,6 @@
+""" # Frame plotting
+Used mainly by Frame.plot_interactive() but could also be used on its own. 
+"""
 import plotly.express as px
 
 intensity_scale = [[0, "black"], [0.1, "blue"], [0.2, "green"], [1, "red"]]
@@ -10,6 +13,21 @@ colorscales = {
 def plotly_3d(
     frame, color: str, point_size: float = 2, **kwargs,
 ):
+    """Plot a Frame as a 3D scatter plot with plotly.
+
+    You can pass arguments to the plotly express function scatter_3D
+
+    Args:
+        frame (Frame): the frame to plot
+        color (str): Which column to plot. For example "intensity"
+        point_size (float, optional): Size of each point. Defaults to 2.
+
+    Raises:
+        ValueError: if the color is not in the data
+
+    Returns:
+        Plotly plot: The interactive plotly plot, best use inside a jupyter notebook.
+    """
     if color not in frame.data.columns:
         raise ValueError(f"choose any of {list(frame.data.columns)}")
 
@@ -34,4 +52,12 @@ def plotly_3d(
 
 
 def pyntcloud_3d(frame, **kwargs):
+    """Plot a Frame with the build in function of pyntcloud. Is faster than plotly.
+
+    Args:
+        frame (Frame): the frame to plot
+
+    Returns:
+        pyntcloud plot: The interactive pyntcloud plot, best use inside a jupyter notebook.
+    """
     return frame.points.plot(mesh=True, backend="threejs", **kwargs)
