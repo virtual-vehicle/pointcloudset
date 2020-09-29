5448642a636a515c3520f9fd344e1551b1ba1c78
thomasgoelles <thomas.goelles@v2c2.at>
2020-09-29 11:45:34 +0000

    moved pipeline to processing

---
+++ lidar/processing/__int__.py
@@ -0,0 +1,2 @@
+"""Processsing routines for Frames or list of Frames which are not part of the
+Frame class itself."""
--- tests/unit/test_pipeline.py
+++ tests/unit/test_pipeline.py
@@ -1,15 +1,17 @@
-import pytest
 import pytest_check as check
 
 from lidar import Dataset, Frame
+from lidar.processing.pipeline import apply_pipeline
 
 
 def test_pipeline1(testset: Dataset):
-    def pipelin1(frame: Frame):
+    test_list = [testset[0], testset[1]]
+
+    def pipeline1(frame: Frame):
         return frame.limit("x", 0, 1)
 
-    testset_result = testset.apply_pipeline(pipeline=pipelin1)
+    testset_result = apply_pipeline(test_list, pipeline=pipeline1)
     check.equal(type(testset_result), list)
-    check.equal(len(testset_result), len(testset))
+    check.equal(len(testset_result), len(test_list))
     check.less_equal(max(testset_result[0].data["x"]), 1.0)
     check.less_equal(max(testset_result[1].data["x"]), 1.0)
