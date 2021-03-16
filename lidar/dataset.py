96496cc06d9600753f04a83e2f37ec7f9032af6d
thomasgoelles <thomas.goelles@v2c2.at>
2021-03-16 08:33:21 +0000

    apply method WIP

--- lidar/dataset.py
+++ lidar/dataset.py
@@ -8,17 +8,30 @@ For more details on how to use it please refer to the usage.ipynb Notebook for a
 * The important stuff happens in the __getitem__ method. Only then the rosbag is actually read with the help of
 generators.
 """
+from __future__ import annotations
 from pathlib import Path
 
 from .dataset_core import DatasetCore
 from .io import DATASET_FROM_FILE, DATASET_TO_FILE
 
-from typing import Callable, TYPE_CHECKING, Optional, List, Any, Union
-import itertools
-from tqdm import tqdm
+from typing import Callable, Optional, Union, get_type_hints, Any
+from dask import delayed
+import dask
 
 
 from .frame import Frame
+
+
+def _is_pipline_returing_frame(pipeline) -> bool:
+    type_hints = get_type_hints(pipeline)
+    res = False
+    if "return" in type_hints:
+        res = get_type_hints(pipeline)["return"] == Frame
+    else:
+        print(
+            "No return type was defined of the pipeline: will not return a new datset"
+        )
+    return res
 
 
 class Dataset(DatasetCore):
@@ -56,35 +69,63 @@ class Dataset(DatasetCore):
     ) -> None:
         DATASET_TO_FILE["DIR"](self, file_path=file_path)
 
-    def apply_pipeline(
+    def apply(
         self,
-        pipeline,
-        start_frame_number: Optional[int] = 0,
+        func: Union[Callable[[Frame], Frame], Callable[[Frame], Any]],
+        start_frame_number: int = 0,
         end_frame_number: Optional[int] = None,
-    ) -> Any:
-        """Applies a function to all, or a given range, of Frames in the dataset.
+    ) -> Union[Dataset, tuple]:
+        """Applies a function onto the dataset.
 
         Example:
 
-        def pipeline1(frame: pd.Dataframe, frame_number: int):
-            return frame.limit("x", 0, 1)
+        def func(frame:lidar.Frame) -> lidar.Frame:
+            return frame.limit(x,0,1)
 
-        test_dataset.apply_pipeline(pipeline1, 0, 10)
+        dataset.apply(func)
+
+        This results in a new dataset
+
+        Example2:
+
+        def func(frame:lidar.Frame) -> float:
+            return frame.data.x.max()
+
+        dataset.apply(func)
 
         Args:
-            pipeline (Callable[[Frame], Frame]): A function with a chain of processings on frames.
-            start_frame_number (int, optional): Frame number to start. Defaults to 0.
-            end_frame_number (Optional, optional): Frame number to end. Defaults to None which corresponds to the end of the dataset.
+            func (Union[Callable[[Frame], Frame], Callable[[Frame], Any]]): [description]
+            start_frame_number (int, optional): Frame number of where to start. Defaults to 0.
+            end_frame_number (Optional[int], optional): Frame number to stop. Defaults to None.
 
         Returns:
-            Any: depends on the pipeline functions
+            Union[Dataset, Any]: A dataset if the function retunrs a Frame object or tuple
+            with the results
         """
 
-        # use dask apply
-        # maybe make a decorator for a pipeline function. The prefered way is that it can
-        # return a new Dataset
+        returns_frame = _is_pipline_returing_frame(func)
+
+        if returns_frame:
+
+            def pipeline_delayed(element_in):
+                frame = Frame(element_in)
+                frame = func(frame)
+                return frame.data
+
+        else:
+
+            def pipeline_delayed(element_in):
+                frame = Frame(element_in)
+                return func(frame)
+
         if end_frame_number is None:
             end_frame_number = len(self)
-        self.data.map_partitions(
-            pipeline,
-        )
+        res = []
+        for i in range(start_frame_number, end_frame_number):
+            item = delayed(pipeline_delayed)(self.data[i])
+            res.append(item)
+
+        if returns_frame:
+            return Dataset(data=res, timestamps=self.timestamps, meta=self.meta)
+        else:
+            return dask.compute(*res)
