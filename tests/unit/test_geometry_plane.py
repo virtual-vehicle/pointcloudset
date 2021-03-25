7c1254f8783e7da8696c666bf5a6c244aa07b66d
Birgit Schlager <birgit.schlager@v2c2.at>
2021-03-25 07:11:31 +0000

    func added: plane intersection with line of sight

--- tests/unit/test_geometry_plane.py
+++ tests/unit/test_geometry_plane.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.testing import assert_array_equal
 import pytest
 import pytest_check as check
 
@@ -23,6 +24,34 @@ def test_distance_to_point_error2():
     with pytest.raises(ValueError):
         plane.distance_to_point(
             point=np.array([1, 0, 0]),
+            plane_model=np.array(
+                [
+                    1,
+                    0,
+                    0,
+                ]
+            ),
+        )
+
+
+def test_intersect_line_of_sight():
+    point = plane.intersect_line_of_sight(
+        line=np.array([1, 1, 1]), plane_model=np.array([0, 0, 1, -1])
+    )
+    np.testing.assert_allclose(point, np.array([1.0, 1.0, 1.0]))
+
+
+def test_intersect_line_of_sight_error1():
+    with pytest.raises(ValueError):
+        plane.intersect_line_of_sight(
+            line=np.array([1, 0]), plane_model=np.array([1, 0, 0, 0])
+        )
+
+
+def test_intersect_line_of_sight_error2():
+    with pytest.raises(ValueError):
+        plane.intersect_line_of_sight(
+            line=np.array([1, 0, 0]),
             plane_model=np.array(
                 [
                     1,
