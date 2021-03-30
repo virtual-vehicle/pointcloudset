294c7a74995319bdd97bb70a796e6148f1adc09c
Birgit Schlager <birgit.schlager@v2c2.at>
2021-03-30 10:05:55 +0000

    added: diff_plane in direction of line of sight

--- tests/unit/test_geometry_plane.py
+++ tests/unit/test_geometry_plane.py
@@ -6,9 +6,27 @@ import pytest_check as check
 from lidar.geometry import plane
 
 
-def test_distance_to_point():
+def test_distance_to_point1():
     distance = plane.distance_to_point(
-        point=np.array([1, 0, 0]), plane_model=np.array([1, 0, 0, 0])
+        point_A=np.array([1, 0, 0]), plane_model=np.array([1, 0, 0, 0])
+    )
+    check.equal(distance, 1.0)
+
+
+def test_distance_to_point2():
+    distance = plane.distance_to_point(
+        point_A=np.array([2, 2, 0]),
+        plane_model=np.array([1, 0, 0, -1]),
+        normal_dist=False,
+    )
+    check.equal(distance, (np.sqrt(2)))
+
+
+def test_distance_to_point3():
+    distance = plane.distance_to_point(
+        point_A=np.array([2, 2, 0]),
+        plane_model=np.array([1, 0, 0, -1]),
+        normal_dist=True,
     )
     check.equal(distance, 1.0)
 
@@ -16,14 +34,14 @@ def test_distance_to_point():
 def test_distance_to_point_error1():
     with pytest.raises(ValueError):
         plane.distance_to_point(
-            point=np.array([1, 0]), plane_model=np.array([1, 0, 0, 0])
+            point_A=np.array([1, 0]), plane_model=np.array([1, 0, 0, 0])
         )
 
 
 def test_distance_to_point_error2():
     with pytest.raises(ValueError):
         plane.distance_to_point(
-            point=np.array([1, 0, 0]),
+            point_A=np.array([1, 0, 0]),
             plane_model=np.array(
                 [
                     1,
