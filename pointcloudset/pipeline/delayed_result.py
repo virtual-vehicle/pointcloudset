7eea7f480015d7a6d5c8109b5601d82a8aa4768f
thomasgoelles <thomas.goelles@v2c2.at>
2021-04-29 07:55:28 +0000

    test of delayed result

--- pointcloudset/pipeline/delayed_result.py
+++ pointcloudset/pipeline/delayed_result.py
@@ -16,9 +16,7 @@ class DelayedResult(UserList):
     def __getitem__(
         self, pointcloud_number: Union[slice, int]
     ) -> Union[DelayedResult, Any]:
-        if isinstance(pointcloud_number, slice):
-            return self[pointcloud_number]
-        elif isinstance(pointcloud_number, int):
+        if isinstance(pointcloud_number, (slice, int)):
             return super().__getitem__(pointcloud_number).compute()
         else:
             raise TypeError("Wrong type {}".format(type(pointcloud_number).__name__))
---
+++ tests/unit/test_pipeline.py
@@ -0,0 +1,36 @@
+import pointcloudset as pcs
+import pytest_check as check
+from pointcloudset.pipeline.delayed_result import DelayedResult
+from typing import Any
+import pytest
+
+
+def first(pc: pcs.PointCloud) -> Any:
+    return pc.data.iloc[0]
+
+
+@pytest.fixture
+def res(testdataset_mini_real):
+    return testdataset_mini_real.apply(first, warn=False)
+
+
+def test_delayed_result(res):
+    res_computed = res.compute()
+    check.is_instance(res, DelayedResult)
+    check.equal(res_computed[0]["x"], 0.9805122017860413)
+
+
+def test_delayed_result0(res):
+    res0 = res[0]
+    check.equal(res0["x"], 0.9805122017860413)
+
+
+def test_delayed_result_wrong(res):
+    with pytest.raises(TypeError):
+        res0 = res["nix"]
+
+
+def test_delayed_result_slice(res):
+    res02 = res[0:2]
+    check.is_instance(res02, list)
+    check.equal(res02[0]["x"], 0.9805122017860413)
