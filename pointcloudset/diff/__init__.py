edd611390cbbe36aded109f258bf9958e9e27977
thomasgoelles <thomas.goelles@v2c2.at>
2021-05-19 13:52:25 +0000

    sort imports

--- pointcloudset/diff/__init__.py
+++ pointcloudset/diff/__init__.py
@@ -2,10 +2,10 @@
 Functions to calculate differences and distances between entities.
 """
 
-from pointcloudset.diff.pointcloud import calculate_distance_to_pointcloud
 from pointcloudset.diff.origin import calculate_distance_to_origin
 from pointcloudset.diff.plane import calculate_distance_to_plane
 from pointcloudset.diff.point import calculate_distance_to_point
+from pointcloudset.diff.pointcloud import calculate_distance_to_pointcloud
 
 ALL_DIFFS = {
     "pointcloud": calculate_distance_to_pointcloud,
