bf7e285d0ece16961cf20049c6773a1d3e9cbc49
thomasgoelles <thomas.goelles@gmail.com>
2022-02-22 07:58:57 +0000

    replaced tqdm with rich

--- pointcloudset/io/dataset/commandline.py
+++ pointcloudset/io/dataset/commandline.py
@@ -1,8 +1,10 @@
 from pathlib import Path
-from typing import Optional, Literal
+from typing import Optional
 
 import click  # needed for documentation
 import typer
+from rich.console import Console
+
 
 from pointcloudset import Dataset
 from pointcloudset.io.dataset.bag import dataset_from_rosbag
@@ -10,6 +12,7 @@ from pointcloudset.io.dataset.bag import dataset_from_rosbag
 from pyntcloud.io import TO_FILE
 
 app = typer.Typer()
+console = Console()
 
 TO_FILE_PYNTCLOUD = list(TO_FILE.keys())
 TO_FILE_CLI = TO_FILE_PYNTCLOUD.append("POINTCLOUDSET")
@@ -51,54 +54,93 @@ def get(
     """The main CLI function to convert ROS bagfiles to pointcloudset or files supported
     by pyntloud.
     """
+    bagfile_paths = _gen_bagfile_paths(bagfile)
+    with console.status("Converting...", spinner="runner"):
+        for bagfile_path in bagfile_paths:
+            typer.echo(f"converting {bagfile_path.name} ...")
+
+            folder_to_write_path = _gen_folder(folder_to_write, bagfile_path)
+
+            typer.echo(f"Writing to {folder_to_write_path}")
+            if output_format == "POINTCLOUDSET":
+                _convert_bag2dir(
+                    bagfile=bagfile_path,
+                    topic=topic,
+                    folder_to_write=folder_to_write_path,
+                    start_frame_number=start_frame_number,
+                    end_frame_number=end_frame_number,
+                    keep_zeros=keep_zeros,
+                    max_size=max_size,
+                    in_loop_function=_in_loop_for_cli,
+                )
+            elif output_format.upper() in TO_FILE_PYNTCLOUD:
+                _convert_bag2files(
+                    topic,
+                    start_frame_number,
+                    end_frame_number,
+                    output_format,
+                    bagfile_path,
+                    folder_to_write_path,
+                )
+            else:
+                raise typer.BadParameter(f"only one of {TO_FILE_CLI} is allowed")
+    console.print("Done :sake:")
+
+
+def _gen_bagfile_paths(bagfile):
     if bagfile == ".":
         bagfile_paths = list(Path.cwd().rglob("*.bag"))
     else:
         bagfile_paths = [Path(bagfile)]
+    return bagfile_paths
 
-    for bagfile_path in bagfile_paths:
-        typer.echo(f"converting {bagfile_path.name} ...")
-        if folder_to_write == ".":
-            folder_to_write_path = Path.cwd().joinpath(bagfile_path.stem)
-        else:
-            folder_to_write_path = Path(folder_to_write).joinpath(bagfile_path.stem)
 
-        if not folder_to_write_path.exists():
-            folder_to_write_path.mkdir(parents=True, exist_ok=False)
-        typer.echo(f"Writing to {folder_to_write_path}")
-        if output_format == "POINTCLOUDSET":
-            _convert_bag2dir(
-                bagfile=bagfile_path,
-                topic=topic,
-                folder_to_write=folder_to_write_path,
-                start_frame_number=start_frame_number,
-                end_frame_number=end_frame_number,
-                keep_zeros=keep_zeros,
-                max_size=max_size,
-                in_loop_function=_in_loop_for_cli,
-            )
-        elif output_format.upper() in TO_FILE_PYNTCLOUD:
-            dataset = Dataset.from_file(
-                file_path=bagfile_path,
-                topic=topic,
-                keep_zeros=False,
-                start_frame_number=start_frame_number,
-                end_frame_number=end_frame_number,
-            )
-            if end_frame_number is None:
-                end_frame_number = len(dataset)
-            for frame in range(start_frame_number, end_frame_number):
-                pyntcloud = dataset[frame].to_instance("PYNTCLOUD")
-                orig_file = Path(bagfile_path).stem
-                filename = folder_to_write_path.joinpath(
-                    f"{orig_file}_{frame}.{output_format.lower()}"
-                )
-                typer.echo(f"writing file {filename} of frame{frame}")
-                pyntcloud.to_file(filename.as_posix())
+def _gen_folder(folder_to_write, bagfile_path):
+    if folder_to_write == ".":
+        folder_to_write_path = Path.cwd().joinpath(bagfile_path.stem)
+    else:
+        folder_to_write_path = Path(folder_to_write).joinpath(bagfile_path.stem)
 
-        else:
-            raise typer.BadParameter(f"only one of {TO_FILE_CLI} is allowed")
-    typer.echo("done")
+    if not folder_to_write_path.exists():
+        folder_to_write_path.mkdir(parents=True, exist_ok=False)
+    return folder_to_write_path
+
+
+def _convert_bag2files(
+    topic,
+    start_frame_number,
+    end_frame_number,
+    output_format,
+    bagfile_path,
+    folder_to_write_path,
+):
+    """Converting a bagfile to files for each frame. Using pyntcloud
+
+    Args:
+        topic (_type_): _description_
+        start_frame_number (_type_): _description_
+        end_frame_number (_type_): _description_
+        output_format (_type_): _description_
+        bagfile_path (_type_): _description_
+        folder_to_write_path (_type_): _description_
+    """
+    dataset = Dataset.from_file(
+        file_path=bagfile_path,
+        topic=topic,
+        keep_zeros=False,
+        start_frame_number=start_frame_number,
+        end_frame_number=end_frame_number,
+    )
+    if end_frame_number is None:
+        end_frame_number = len(dataset)
+    for frame in range(start_frame_number, end_frame_number):
+        pyntcloud = dataset[frame].to_instance("PYNTCLOUD")
+        orig_file = Path(bagfile_path).stem
+        filename = folder_to_write_path.joinpath(
+            f"{orig_file}_{frame}.{output_format.lower()}"
+        )
+        typer.echo(f"writing file {filename} of frame{frame}")
+        pyntcloud.to_file(filename.as_posix())
 
 
 typer_click_object = typer.main.get_command(app)
