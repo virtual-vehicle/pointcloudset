6b63f8340adcd15bf72f34c11dbd0111777629df
thomasgoelles <thomas.goelles@gmail.com>
2022-04-15 09:05:30 +0200

    updating to open3d 0.15

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "numpy",
         "pandas",
         "pyntcloud",
-        "open3d>=0.14.0",
+        "open3d>=0.15.1",
         "plotly",
         "fastparquet",
         "dask>=2022.02.0",
