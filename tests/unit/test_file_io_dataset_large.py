93f1a3c228e439e5499276cf9b4f9eb6fadd18e3
thomasgoelles <thomas.goelles@gmail.com>
2022-09-19 11:35:05 +0000

    better testing for large files

--- tests/unit/test_file_io_dataset_large.py
+++ tests/unit/test_file_io_dataset_large.py
@@ -1,23 +1,34 @@
-# test on large fieles
+# test on large files
 # testfiles are not int the github repo since they are too large
-
-from datetime import datetime
 from pathlib import Path
 
-import numpy as np
-import pandas as pd
 import pytest
 import pytest_check as check
-from pandas._testing import assert_frame_equal
-from pointcloudset import Dataset, PointCloud
-from pointcloudset.io.dataset import dir
+from pointcloudset import Dataset
 
 
-@pytest.mark.parametrize("filename", ["big_comp.bag", "big_uncomp.bag"])
-def test_read_bag_big(testdata_path_large: Path, filename):
+@pytest.mark.parametrize("filename", ["big_comp.bag", "big_uncomp.bag", "big_1000.bag"])
+def test_read_and_write_bag_big(testdata_path_large: Path, filename, tmp_path: Path):
     if testdata_path_large.exists():
-        dataset = Dataset.from_file(
-            testdata_path_large.joinpath(filename), topic="/os1_cloud_node/points"
+        # read
+        ds = Dataset.from_file(
+            testdata_path_large.joinpath(filename),
+            topic="/os1_cloud_node/points",
+            keep_zeros=True,
         )
-        check.is_instance(dataset, Dataset)
-        check.equal(len(dataset), 250)
+        check.is_instance(ds, Dataset)
+        check.equal(len(ds), 250)
+        testfile_name = tmp_path.joinpath("dataset")
+        # write
+        ds.to_file(file_path=testfile_name, use_orig_filename=False)
+        p = testfile_name.glob("*.parquet")
+        files = [x for x in p if x.is_file()]
+        check.equal(len(files), 250)
+        meta_gen = testfile_name.glob("meta.json")
+        metafile = list(meta_gen)[0]
+        check.equal(metafile.exists(), True)
+        check.equal(testfile_name.exists(), True)
+        # read again
+        read_dataset = Dataset.from_file(testfile_name)
+        check.is_instance(read_dataset, Dataset)
+        check.equal(len(ds), len(read_dataset))
